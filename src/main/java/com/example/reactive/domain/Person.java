280705f121e7d346918c9ff9d661832a95c377c6
Marco Seabra <marcogutto@brq.com>
2021-12-17 19:40:07 -0300

    Ajuste da API

--- src/main/java/com/example/reactive/domain/Person.java
+++ src/main/java/com/example/reactive/domain/Person.java
@@ -1,22 +1,59 @@
 package com.example.reactive.domain;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
+import javax.ws.rs.NotFoundException;
+
+import org.bson.codecs.pojo.annotations.BsonId;
 import org.bson.codecs.pojo.annotations.BsonProperty;
+import org.bson.types.ObjectId;
 
-import io.quarkus.mongodb.panache.reactive.ReactivePanacheMongoEntity;
+import io.quarkus.mongodb.panache.reactive.ReactivePanacheMongoEntityBase;
+import io.smallrye.mutiny.Multi;
+import io.smallrye.mutiny.Uni;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @Data
 @EqualsAndHashCode(callSuper=false)
 
-public class Person extends ReactivePanacheMongoEntity {
+public class Person extends ReactivePanacheMongoEntityBase {
+    
+    @BsonId
+	public String id;
 
-	public String name;
+    public String name;
 
     @BsonProperty("birth")
     public LocalDate birthDate;
 
     public Status status;
+
+    public LocalDateTime creationDate;
+
+    public static Multi<Person> streamAllPerson() {
+        return streamAll();
+    }
+
+    public static Uni<Person> update(String id, Person updatePerson) {
+        Uni<Person> personUni = Person.findById(id);
+        return personUni
+                .onItem().transform(person -> {
+                    person.setName(updatePerson.getName());
+                    person.setBirthDate(updatePerson.getBirthDate());
+                    person.setStatus(updatePerson.getStatus());
+                    return person;
+                }).call(person -> person.persistOrUpdate());
+    }
+
+    public static Uni<Void> delete(String id) {
+        Uni<Person> personUni = findById(id);
+
+        return personUni
+                .onItem().ifNull().failWith(() -> new NotFoundException())
+                .chain(person -> {
+                    return person.delete();
+                });
+    }
 }
--- src/main/java/com/example/reactive/resource/PersonResource.java
+++ src/main/java/com/example/reactive/resource/PersonResource.java
@@ -1,7 +1,8 @@
 package com.example.reactive.resource;
 
-import org.bson.types.ObjectId;
-
+import java.net.URI;
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
 import java.util.List;
 
 import javax.ws.rs.Consumes;
@@ -12,56 +13,89 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import com.example.reactive.domain.Person;
+import com.example.reactive.domain.Status;
 
+import org.bson.types.ObjectId;
+
+import io.smallrye.mutiny.Multi;
 import io.smallrye.mutiny.Uni;
 
 @Path("/persons")
 public class PersonResource {
 
     @GET
-    public Uni<List<Person>> list() {
-        return Person.listAll();
+    @Produces(MediaType.APPLICATION_JSON)
+    public Multi<Person> stream() {
+        return Person.streamAllPerson();
     }
 
     @GET
     @Path("/{id}")
-    public Uni<Person> get(@PathParam("id") String id) {
+    @Produces(MediaType.APPLICATION_JSON)
+    public Uni<Person> findById(@PathParam("id") String id) {
         return Person.findById(id);
     }
 
+    @GET
+    @Path("/search")
+    public Uni<List<Person>> search(@QueryParam("name") String name, @QueryParam("status") Status status,
+                                  @QueryParam("dateFrom") String dateFrom, @QueryParam("dateTo") String dateTo) {
+        if (name != null) {
+            return Person.find("{'name': ?1,'status': ?2}", name, status).list();
+        }
+        return Person
+                .find("{'creationDate': {$gte: ?1}, 'creationDate': {$lte: ?2}}", ZonedDateTime.parse(dateFrom).toLocalDateTime(),
+                        ZonedDateTime.parse(dateTo).toLocalDateTime()).list();
+    }
+
     @POST
+    @Consumes(MediaType.APPLICATION_JSON)
     public Uni<Response> create(Person person) {
-        person.persist();
-        return Uni.createFrom().item(Response.status(201).build());
+        person.creationDate = LocalDateTime.now();
+        return person.<Person>persist().map(v ->
+                Response.created(URI.create("/persons/" + v.id.toString()))
+                        .entity(person).build());
     }
 
     @PUT
     @Path("/{id}")
-    public void update(@PathParam("id") String id, Person person) {
-        person.update();
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Uni<Person> update(@PathParam("id") String id, Person person) {
+        return Person.update(id, person);
     }
 
     @DELETE
     @Path("/{id}")
-    public void delete(@PathParam("id") String id) {
-        Person.findById(id).subscribe().with(person -> {
-
-            person.delete();
-
-        });
+    public Uni<Void> delete(@PathParam("id") String id) {
+        return Person.delete(id);
     }
 
     // @GET
-    // @Path("/search/{name}")
-    // public Uni<Person> search(@PathParam("name") String name) {
-    //     return Person.findByName(name);
+    // @Path("/search2")
+    // public Uni<List<Post>> searchCustomQueries(@QueryParam("authors") List<String> authors) {
+
+        // using Document
+        // return Post.find(new Document("author", new Document("$in", authors))).list();
+
+        // using a raw JSON query
+        //Post.find("{'$or': {'author':John Doe, 'author':Grace Kelly}}");
+        //Post.find("{'author': {'$in': [John Doe, Grace Kelly]}}");
+
+        // using Panache QL
+        //Post.find("author in (John Doe,Grace Kelly)");
+
     // }
 
-    @DELETE
-    public void deleteAll(){
-        Person.deleteAll();
-    }
+    // @PUT
+    // @Path("/{id}/comment")
+    // @Consumes(MediaType.APPLICATION_JSON)
+    // @Produces(MediaType.APPLICATION_JSON)
+    // public Uni<Response> addCommentToPost(@PathParam("id") String id, Comment comment) {
+    //     return Post.addCommentToPost(comment, id).map(v -> Response.accepted(v).build());
+    // }
 }
